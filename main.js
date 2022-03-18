f5af2e048431c492a5c33c2907dc1ba69d2bd19e
Andy <andy@AM-Mac.local>
2022-03-18 17:46:32 +0000

    Updated with working pulses

--- main.js
+++ main.js
@@ -6,21 +6,58 @@
 
 import V3Store from "/vee3/vee_store.js";
 import V3Instance from "/vee3/vee_instance.js";
+import V3Contact from "/vee3/vee_contact.js";
 
 import Tests from "./tests.js";
 
 class Model {
     constructor(api) {
-        // name App name
-        this.name = ko.observable("testopen");
-        // webid The webid (set only if logged in)
         this.webid = ko.observable("");
+        this.contacts = ko.observableArray([]);
+        this.txt_userid = ko.observable("");
 
-        this.btn_run_tests = async() => {
-            mocha.run();
+        this.refresh = async () => {
+            let list1 = await V3Contact.$list();
+            if(list1.status === "ok"){
+                this.contacts(list1.body.contacts);
+            }
         }
 
-        this.tests = new Tests();
+        this.on_add = async (data,evt) => {
+            // Add the contact
+            await this.add(this.txt_userid());
+            // reset the input
+            this.txt_userid("");
+        }
+
+        this.add = async (contact_id) => {
+            let contact1 = new V3Contact.$Contact({name:contact_id, contact_id:contact_id});
+            let create1 = await V3Contact.$create(contact1);
+            // Check results
+            if(create1.status === "ok"){
+                console.log(`OK: ${create1.msg}`);
+            } else {
+                console.log(`ERROR: ${create1.msg}`);
+            }
+            // Refresh the contacts
+            this.refresh();
+        }
+
+        this.on_remove = async (data,evt) => {
+            this.remove(data.contact_id);
+        }
+
+        this.remove = async (contact_id) => {
+            // Delete the contact by ID
+            let delete1 = await V3Contact.$delete(contact_id);
+            if(delete1.ok === true){
+                console.log(`OK: ${delete1.msg}`);
+            } else {
+                console.log(`ERROR: ${delete1.msg}`);
+            }
+            // Refresh the contacts
+            this.refresh();
+        }
     }
 }
 
@@ -28,7 +65,7 @@ export default class Main {
     constructor(config) {
         try {
             // Set instanceid in the V3Store
-            V3Store.instanceId(config.app.instancedid);
+            // V3Store.instanceId(config.app.instancedid);
             V3Instance.instanceId(config.app.instancedid);
             // Create model
             this.model = new Model(this.api);
@@ -41,5 +78,7 @@ export default class Main {
         }
     }
 
-    async init(config) {}
+    async init(config) {
+        this.model.refresh();
+    }
 }
